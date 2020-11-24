f699bf3901032183097c173139f613eac15ccef9
David Forrest <drf5na@gmail.com>
2020-11-24 12:33:06 -0500

    Add YorkCountyCovidMetric.py script

---
+++ YorkCountyCovidMetric.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Time Series of YCSD Covid Case Metric
+# 
+# This notebook examines the time series of the York County COVID Cases ostensibly used by YCSD to make decisions about school reopenings.
+# 
+# * YCSD presents there metrics on https://ycsd.yorkcountyschools.org/domain/1313
+# * VDH represents the this number for the localities on https://www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-locality/ and on https://www.vdh.virginia.gov/coronavirus/key-measures/pandemic-metrics/school-metrics/ under the localities tab
+# * VDH shares the data at https://data.virginia.gov/Government/VDH-COVID-19-PublicUseDataset-Cases/bre9-aqqr
+# * I'm sharing This notebook in Github at https://github.com/drf5n/YCSD_covid_metrics
+# 
+# -- David Forrest
+# 
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'widget')
+import os,sys,io, time, pathlib
+import pandas as pd
+#import numpy as np, matplotlib as mpl, matplotlib.pyplot as plt
+
+import bokeh.plotting
+import bokeh.io
+import bokeh.models
+from bokeh.io import output_notebook
+bokeh.io.output_notebook()
+
+
+# In[2]:
+
+
+def file_age(filepath):
+    return time.time() - os.path.getmtime(filepath)
+
+
+# In[3]:
+
+
+# get the Virginia COVID Case data from https://data.virginia.gov/Government/VDH-COVID-19-PublicUseDataset-Cases/bre9-aqqr
+
+df_name = "VA_vdh_casedata.csv"
+if file_age(df_name) > 86400:
+    get_ipython().system("wget -O $df_name 'https://data.virginia.gov/api/views/bre9-aqqr/rows.csv?accessType=DOWNLOAD'")
+    pathlib(dfname).touch()
+
+
+# In[4]:
+
+
+df=pd.read_csv(df_name)
+df["date"] = pd.to_datetime(df['Report Date'])
+
+df.tail()
+
+
+# In[5]:
+
+
+
+df = df.sort_values(by=['Locality', 'VDH Health District', 'date'])
+display(df.head())
+
+df['TC_diff']= df.groupby('Locality')['Total Cases'].diff().fillna(0)
+df['TC_sum14']= df.groupby('Locality')['Total Cases'].diff(14).fillna(0)
+
+display(df.tail())
+
+
+# In[6]:
+
+
+# subset for York and normalize per capita
+dfy = df[df['Locality']=='York'].copy()
+dfy['per100k_14daysum']=dfy['TC_sum14']*100000/67782  
+
+
+# In[7]:
+
+
+dfy
+
+
+# In[8]:
+
+
+ph = dfy.plot(y='per100k_14daysum',x='date',title="York County Number of new cases per 100,000 persons \nwithin the last 14 days")
+
+ph
+
+
+# In[9]:
+
+
+ph = dfy.plot(y='TC_diff',x='date',title="York County Cases, 14 day sum, per 100K")
+ph
+
+
+# In[10]:
+
+
+TOOLTIPS = [
+ #   ("index", "$index"),
+ #   ("date:", "$x{%F %T}"),
+    ("date:", "@date{%F}"),
+    ("cases/14d/100k:","@per100k_14daysum"),
+ #   ("(x,y)", "($x, $y)"),
+]
+
+
+#p=bokeh.plotting.figure( tooltips=TOOLTIPS, x_axis_type='datetime')
+p=bokeh.plotting.figure( x_axis_type='datetime',y_range=(0,250),
+#                        tooltips=TOOLTIPS,formatters={"$x": "datetime"},
+                        title="York County Number of new cases per 100,000 persons within the last 14 days")
+
+    
+hth = bokeh.models.HoverTool(tooltips=TOOLTIPS,
+                             formatters={"$x": "datetime",
+                                        "@date": "datetime"
+                                        },
+                             mode='vline',
+                            )
+
+print(hth)
+print(hth.formatters)
+p.add_tools(hth)
+#hover = p.select(dict(type=bokeh.models.HoverTool))
+
+
+#hover(tooltips=TOOLTIPS,
+#)
+
+p.add_layout(bokeh.models.BoxAnnotation(bottom=0,top=5, fill_alpha=0.4, fill_color='olive'))
+p.add_layout(bokeh.models.BoxAnnotation(bottom=5,top=20, fill_alpha=0.4, fill_color='green'))
+p.add_layout(bokeh.models.BoxAnnotation(bottom=20,top=50, fill_alpha=0.4, fill_color='yellow'))
+p.add_layout(bokeh.models.BoxAnnotation(bottom=50,top=200, fill_alpha=0.4, fill_color='orange'))
+p.add_layout(bokeh.models.BoxAnnotation(bottom=200, fill_alpha=0.4, fill_color='red'))
+
+
+
+#p.line(dfy['date'],dfy['per100k_14daysum'])
+p.line(x='date', y='per100k_14daysum',source=dfy)
+#p.title()
+
+#?p.line
+
+
+# In[11]:
+
+
+bokeh.plotting.show(p)
+
+
+# In[12]:
+
+
+bokeh.plotting.output_file('YorkCountyCovidMetric_plot.html', mode='inline')
+bokeh.plotting.save(p)
+
+# needs geckodriver  -- have it in conda env py3plot
+bokeh.io.export_png(p, filename="YorkCountyCovidMetric_plot.png")
+
+
+# In[13]:
+
+
+#
+get_ipython().system(' jupyter nbconvert --to script *.ipynb')
+
+
+# In[ ]:
+
+
+
+
