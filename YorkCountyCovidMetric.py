bba0b3558cfc82ca0e596895acd57a5044e68738
David Forrest <drf5na@gmail.com>
2021-02-19 12:00:21 -0500

    Update for 2021-02-19

--- YorkCountyCovidMetric.py
+++ YorkCountyCovidMetric.py
@@ -36,7 +36,7 @@ def file_age(filepath):
     return time.time() - os.path.getmtime(filepath)
 
 
-# In[19]:
+# In[20]:
 
 
 # get the Virginia COVID Case data from https://data.virginia.gov/Government/VDH-COVID-19-PublicUseDataset-Cases/bre9-aqqr
